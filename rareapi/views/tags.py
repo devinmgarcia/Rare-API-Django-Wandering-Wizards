516a7cbaa7e007105d0c24f80474764cb7b12186
Noah Husain <noahhusain@gmail.com>
2021-08-17 10:34:09 -0500

    Tags can now be deleted by using the destroy method

--- rareapi/views/tags.py
+++ rareapi/views/tags.py
@@ -36,3 +36,21 @@ class TagView(ViewSet):
             tags, many=True, context={'request': request})
         return Response(serializer.data)
 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single post
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            tag = Tag.objects.get(pk=pk)
+            tag.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Tag.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
